9f1d29417fc76ca3cd1362ee7a8d345ddd2fe4d7
Ian Huff <ianhu@microsoft.com>
2021-05-27 11:01:39 -0700

    Fix plotly under ES6 import (#51)
    * String replace to fix up plotly bundle
    
    * add comment
    
    Co-authored-by: Ian Huff <ianhuff@lcs2d-Dell-04.northamerica.corp.microsoft.com>

--- build/webpack/webpack.client.config.js
+++ build/webpack/webpack.client.config.js
@@ -7,6 +7,7 @@ const constants = require('../constants');
 const configFileName = 'src/client/tsconfig.json';
 const { DefinePlugin } = require('webpack');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const StringReplacePlugin = require('string-replace-webpack-plugin');
 // Any build on the CI is considered production mode.
 const isProdBuild = constants.isCI || process.argv.some((argv) => argv.includes('mode') && argv.includes('production'));
 
@@ -36,6 +37,7 @@ module.exports = {
         new DefinePlugin({
             scriptUrl: 'import.meta.url'
         }),
+        new StringReplacePlugin(),
         ...common.getDefaultPlugins('extension')
     ],
     stats: {
@@ -126,6 +128,26 @@ module.exports = {
                 use: [
                     {
                         loader: 'node-loader'
+                    }
+                ]
+            },
+            {
+                test: /plotly\.js$/,
+                use: [
+                    {
+                        // https://github.com/plotly/plotly.js/issues/3518#issuecomment-779758848
+                        // Plotly bundle doesn't work under ES6 import. Using the work around (minified version)
+                        // from the link above
+                        loader: StringReplacePlugin.replace({
+                            replacements: [
+                                {
+                                    pattern: /module.exports = d3; else this.d3 = d3;\n}\(\);/,
+                                    replacement: function () {
+                                        return 'module.exports = d3; else this.d3 = d3;\n}.apply(self);';
+                                    }
+                                }
+                            ]
+                        })
                     }
                 ]
             }
