f07fbc48efb57eb9ae0f7efd4917ee9549778c31
Don Jayamanne <don.jayamanne@outlook.com>
2021-09-16 11:07:00 -0700

    Revert "Dump extension entry point" (#63)

--- build/webpack/common.js
+++ build/webpack/common.js
@@ -1,8 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+const constants = require('../constants');
 const webpack_bundle_analyzer = require('webpack-bundle-analyzer');
 function getDefaultPlugins(name) {
+    if (!constants.isCI && !process.argv.some((argv) => argv.includes('mode') && argv.includes('production'))) {
+        return [];
+    }
     return [
         new webpack_bundle_analyzer.BundleAnalyzerPlugin({
             analyzerMode: 'static',
