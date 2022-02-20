7b7bf5f15649d8060080d37a7e21d451a0f1153b
Don Jayamanne <don.jayamanne@outlook.com>
2022-02-21 10:48:12 +1100

    FIxes

---
+++ build/postInstall.js
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const path = require('path');
+const fs = require('fs');
+
+function fixJupyterLabRenderers() {
+    const filePath = path.join(__dirname, '..', 'node_modules/@jupyterlab/rendermime/lib/renderers.js');
+    const textToReplace = `import marked from 'marked'`;
+    const fileContents = fs.readFileSync(filePath, 'utf8').toString();
+    if (fileContents.indexOf(textToReplace) === -1) {
+        throw new Error('Unable to find Jupyter marked usage to replace!');
+    }
+    fs.writeFileSync(filePath, fileContents.replace(textToReplace, `import { marked } from 'marked'`));
+}
+
+fixJupyterLabRenderers();
