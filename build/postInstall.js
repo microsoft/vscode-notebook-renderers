a4ffbd4ff95794d95854af14c1c35a98a56a9837
Don Jayamanne <don.jayamanne@outlook.com>
2022-02-21 11:50:17 +1100

    More fixes

--- build/postInstall.js
+++ build/postInstall.js
@@ -7,11 +7,12 @@ const fs = require('fs');
 function fixJupyterLabRenderers() {
     const filePath = path.join(__dirname, '..', 'node_modules/@jupyterlab/rendermime/lib/renderers.js');
     const textToReplace = `import marked from 'marked'`;
+    const textToReplaceWith = `import { marked } from 'marked'`;
     const fileContents = fs.readFileSync(filePath, 'utf8').toString();
-    if (fileContents.indexOf(textToReplace) === -1) {
+    if (fileContents.indexOf(textToReplace) === -1 && fileContents.indexOf(textToReplaceWith) === -1) {
         throw new Error('Unable to find Jupyter marked usage to replace!');
     }
-    fs.writeFileSync(filePath, fileContents.replace(textToReplace, `import { marked } from 'marked'`));
+    fs.writeFileSync(filePath, fileContents.replace(textToReplace, textToReplaceWith));
 }
 
 fixJupyterLabRenderers();
