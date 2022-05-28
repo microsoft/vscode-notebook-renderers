96001d6faaf7f5844beaa2d6e6f22db370e38373
Rich Chiodo <rchiodo@users.noreply.github.com>
2022-05-27 17:27:49 -0700

    Fix animations in plotly

--- build/postInstall.js
+++ build/postInstall.js
@@ -15,4 +15,28 @@ function fixJupyterLabRenderers() {
     fs.writeFileSync(filePath, fileContents.replace(textToReplace, textToReplaceWith));
 }
 
+function fixPlotlyRenderer() {
+    const filePath = path.join(__dirname, '..', 'node_modules/@nteract/transform-plotly/lib/index.js');
+    const textsToReplace = [
+        `this.Plotly.newPlot(this.plotDiv, figure.data, figure.layout);`,
+        `const { data = {}, layout = {} } = figure;`,
+        `return { data, layout };`
+    ];
+    const textsToReplaceWith = [
+        `this.Plotly.newPlot(this.plotDiv, figure);`,
+        `const { data = {}, layout = {}, frames = {}, config = {} } = figure;`,
+        `return { data, layout, config, frames };`
+    ];
+    const fileContents = fs.readFileSync(filePath, 'utf8').toString();
+    if (fileContents.indexOf(textsToReplace[0]) === -1) {
+        throw new Error('Unable to find plotly fixup');
+    }
+    let replacedFileContents = fileContents;
+    textsToReplace.forEach((s, i) => {
+        replacedFileContents = replacedFileContents.replace(s, textsToReplaceWith[i]);
+    });
+    fs.writeFileSync(filePath, replacedFileContents);
+}
+
 fixJupyterLabRenderers();
+fixPlotlyRenderer();
