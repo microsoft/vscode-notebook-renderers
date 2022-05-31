7ff339c6fe875d7cdded1d7f2291ef7dec7fbf59
Rich Chiodo <rchiodo@users.noreply.github.com>
2022-05-31 10:51:22 -0700

    Fix animations in plotly (#113)
    * Fix animations in plotly
    
    * Fix npm parse
    
    * Regen package lock
    
    * Fixup package-lock again

--- build/postInstall.js
+++ build/postInstall.js
@@ -15,4 +15,28 @@ function fixJupyterLabRenderers() {
     fs.writeFileSync(filePath, fileContents.replace(textToReplace, textToReplaceWith));
 }
 
+function fixPlotlyRenderer() {
+    const filePath = path.join(__dirname, '..', 'node_modules/@nteract/transform-plotly/lib/index.js');
+    const textsToReplace = [
+        `this.Plotly.newPlot(this.plotDiv, figure.data, figure.layout);`,
+        `const { data = {}, layout = {} } = figure;`,
+        `return { data, layout };`
+    ];
+    const textsToReplaceWith = [
+        `this.Plotly.newPlot(this.plotDiv, figure); //Replaced by postinstall`,
+        `const { data = {}, layout = {}, frames = {}, config = {} } = figure;`,
+        `return { data, layout, config, frames };`
+    ];
+    const fileContents = fs.readFileSync(filePath, 'utf8').toString();
+    if (fileContents.indexOf(textsToReplace[0]) === -1 && fileContents.indexOf(textsToReplaceWith[0]) === -1) {
+        throw new Error('Unable to find plotly fixup');
+    }
+    let replacedFileContents = fileContents;
+    textsToReplace.forEach((s, i) => {
+        replacedFileContents = replacedFileContents.replace(s, textsToReplaceWith[i]);
+    });
+    fs.writeFileSync(filePath, replacedFileContents);
+}
+
 fixJupyterLabRenderers();
+fixPlotlyRenderer();
