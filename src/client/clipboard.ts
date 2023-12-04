0c011b8fdf2b6ba741da9ed10ff1091ab26adfaa
Andreas Kirsch <blackhc@gmail.com>
2023-12-04 21:58:23 +0000

    Add clipboard support for image/webp
    This requires using a canvas object to render the webp to png.
    
    Manually tested in Chrome.

--- src/client/clipboard.ts
+++ src/client/clipboard.ts
@@ -9,11 +9,61 @@ declare interface ClipboardItem {
     getType(type: string): Promise<Blob>;
 }
 
-export async function writeImageToClipboard(blob: Blob) {
-    const item = new ClipboardItem({ 'image/png': blob });
+async function convertWebPToPNG(webpBlob: Blob): Promise<Blob> {
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        const url = URL.createObjectURL(webpBlob);
+
+        image.onload = () => {
+            const canvas = document.createElement('canvas');
+            canvas.width = image.width;
+            canvas.height = image.height;
+
+            const ctx = canvas.getContext('2d');
+            if (!ctx) {
+                reject(new Error('Unable to get canvas context'));
+                return;
+            }
+
+            ctx.drawImage(image, 0, 0);
+
+            canvas.toBlob((pngBlob) => {
+                if (pngBlob) {
+                    resolve(pngBlob);
+                } else {
+                    reject(new Error('Canvas to Blob conversion failed'));
+                }
+            }, 'image/png');
+
+            URL.revokeObjectURL(url);
+        };
+
+        image.onerror = () => {
+            reject(new Error('Error loading WebP image'));
+            URL.revokeObjectURL(url);
+        };
+
+        image.src = url;
+    });
+}
+
+export async function writeImageToClipboard(blob: Blob, mimeType: string) {
+    let imageBlob = blob;
+
+    // Convert WebP to PNG if necessary
+    if (mimeType === 'image/webp') {
+        imageBlob = await convertWebPToPNG(blob);
+        mimeType = 'image/png'; // Update the mimeType to 'image/png'
+    }
+
+    // Create ClipboardItem with the correct mimeType
+    const item = new ClipboardItem({ [mimeType]: imageBlob });
+
+    // Check if clipboard API is supported
     if (!('write' in navigator.clipboard)) {
         throw new Error('navigator.clipboard.write not supported');
     }
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     await (navigator.clipboard as any).write([item]);
 }
