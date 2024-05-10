253ad75db6c0595235b44a3c992d7de7a437c141
Aaron Munger <aamunger@microsoft.com>
2024-05-10 09:15:42 -0700

    remove console logging (#202)

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -20,7 +20,6 @@ import { CellOutput } from './render';
 import { ActivationFunction, OutputItem, RendererContext } from 'vscode-notebook-renderer';
 
 export const activate: ActivationFunction = (ctx: RendererContext<unknown>) => {
-    console.log('Jupyter Notebook Renderer activated');
     return {
         renderOutputItem(outputItem: OutputItem, element: HTMLElement) {
             renderOutput(outputItem, element, ctx);
@@ -39,9 +38,7 @@ function renderOutput(outputItem: OutputItem, element: HTMLElement, ctx: Rendere
         if (!ctx.workspace.isTrusted) {
             return;
         }
-        console.log('request', outputItem);
         const output = convertVSCodeOutputToExecuteResultOrDisplayData(outputItem);
-        console.log(`Rendering mimeType ${mimeString}`, output);
 
         ReactDOM.render(
             React.createElement(CellOutput, { mimeType: mimeString, output, ctx, outputId: outputItem.id }, null),
